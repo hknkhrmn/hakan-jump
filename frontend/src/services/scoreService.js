da8a6f7a7debc94e194b239958be8d6354f52b15
Hakan Kahraman <hakank4hraman@gmail.com>
2026-04-02 15:59:39 +0300

    change

--- frontend/src/services/scoreService.js
+++ frontend/src/services/scoreService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = ' https://hakan-jump.onrender.com/api';
+const API_URL = 'https://hakan-jump.onrender.com/api';
 
 export const scoreService = {
   getScores: async () => {
